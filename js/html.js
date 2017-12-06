0e91aa1bfc65751b10331ed52d9d7c9bc3cef546
keqizhen <kqz0507@gmail.com>
2017-12-06 21:23:00 +0800

    change html/RegExp.html

--- js/html.js
+++ js/html.js
@@ -1,21 +1,33 @@
 $(function () {
-	fn1('.a-attr', '.a-hid', '.a-son-hid');
-	fn1('.btn-url', '.a-url-hid');
-	fn1('.btn-val', '.tar-hid');
-	fn1('.abbr-attr', '.abbr-hid');
-	fn1('.audio-attr', '.audio-hid');
-	function fn1(ele1, ele2, ele3) {
-		$('#main').children('.panel').eq(1).find(ele1).click(function () {
-			if($('#main').children('.panel').eq(1).find(ele2).is(':hidden')){
-				$('#main').children('.panel').eq(1).find(ele2).show();
+	fn('.a-attr', '.a-hid', '.a-son-hid');
+	fn('.btn-url', '.a-url-hid');
+	fn('.btn-val', '.tar-hid');
+	fn('.abbr-attr', '.abbr-hid');
+	fn('.audio-attr', '.audio-hid');
+	fn('.img-attr', '.img-hid', '.img-son-hid');
+	fn('.form-attr', '.form-hid');
+	fn('.input-attr', '.input-hid', '.input-attr-hid');
+	fn('.input-attr-attr', '.input-attr-hid');
+	fn('.laber-attr', '.laber-hid');
+	fn('.fiel-attr', '.fiel-hid');
+	fn('.btn-attr', '.btn-hid');
+	fn('.se-attr', '.se-hid');
+	fn('.opti-attr', '.opti-hid');
+	fn('.optg-attr', '.optg-hid');
+	fn('.textarea-attr', '.textarea-hid');
+	fn('.outp-attr', '.outp-hid');
+	fn('.meta-attr', '.meta-hid');
+	function fn(ele1, ele2, ele3) {
+		$('#main').children('.panel2').find(ele1).click(function () {
+			if($('#main').children('.panel2').find(ele2).is(':hidden')){
+				$('#main').children('.panel2').find(ele2).show();
 			} else{
-				$('#main').children('.panel').eq(1).find(ele2).hide();
-				$('#main').children('.panel').eq(1).find(ele3).hide();
+				$('#main').children('.panel2').find(ele2).hide();
+				$('#main').children('.panel2').find(ele3).hide();
 			}
 		})
 	}
 	$('.a-rel').click(function () {
 		window.open("http://www.w3school.com.cn/tags/att_a_rel.asp");
 	})
-	fn1('.img-attr', '.img-hid', '.img-son-hid');
 })
